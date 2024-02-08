c6f679ab491019de97ab4138a2d537f256ef8cd0
BatLeDev <baptiste.guerny@gmail.com>
2024-02-08 11:21:20 +0100

    feat: change dependency processing-test-utils to @data-fair/lib

--- test/test.js
+++ test/test.js
@@ -1,12 +1,24 @@
 process.env.NODE_ENV = 'test'
 const config = require('config')
-const testUtils = require('@data-fair/processings-test-utils')
+const assert = require('assert').strict
 const rnaProcessing = require('../index.js')
 
 describe('RNA', function () {
+  it('should expose a plugin config schema for super admins', async () => {
+    const schema = require('../plugin-config-schema.json')
+    assert.ok(schema)
+  })
+
+  it('should expose a processing config schema for users', async () => {
+    const schema = require('../processing-config-schema.json')
+    assert.equal(schema.type, 'object')
+  })
+
   it('should download, process files and upload a csv on the staging', async function () {
     this.timeout(1000000)
-    const context = testUtils.context({
+
+    const testsUtils = await import('@data-fair/lib/processings/tests-utils.js')
+    const context = testsUtils.context({
       pluginConfig: {
       },
       processingConfig: {
@@ -14,7 +26,8 @@ describe('RNA', function () {
         datasetMode: 'create',
         dataset: { title: 'RNA test' },
         datasetID: 'repertoire-national-des-associations',
-        forceUpdate: false
+        forceUpdate: false,
+        filter: '56'
       },
       tmpDir: 'data/'
     }, config, false)
